6c4c5afb5dc88ca367a09b13788bbfe9404794f6
LakviduRathnayake <lakviduupasara2001@gmail.com>
2024-10-06 13:20:50 +0530

    update

--- support_membercntrl.php
+++ support_membercntrl.php
@@ -10,11 +10,24 @@ require 'config2.php';
 if ($_SERVER($_POST[ "REQUEST _METHOD" ] == "POST") )
 {
 
-    if(isset($_POST["submit"]))
-    {
-            $sql = "INSERT INTO  tickets Values " ;
-
+    if (isset($_POST['submit'])) {
+        // Retrieve form data
+        $subject = mysqli_real_escape_string($conn, $_POST['Subject']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $message = mysqli_real_escape_string($conn, $_POST['message']);
+    
+        // Insert data into the tickets table
+        $sql = "INSERT INTO tickets (Subject, Email, Message) VALUES ('$subject', '$email', '$message')";
+    
+        if ($conn->query($sql) === TRUE) {
+            echo "New ticket created successfully!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
+    
+    // Close the connection
+    $conn->close();
 
 
 }
