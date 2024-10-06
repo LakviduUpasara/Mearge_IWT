4b4d6f368161949f6c6622709733466303296870
Tnadeeja <thamiyanw@gmail.com>
2024-10-06 06:25:58 +0530

    contactus

--- contactUs.php
+++ contactUs.php
@@ -220,13 +220,34 @@
 </div>
 
 
-    <div class="footer">
+    <!--Footer -->
+    <footer>
+        <div class="footer-col">
+            <h4>Quick Links</h4>
+            <ul>
+            <li><a href="home.php">-Home</a></li>
+            <li><a href="About us.php">-About Us</a></li>
+            <li><a href="#">-Conatct Us</a></li>
+            <li><a href="PrivacyPolicyContent.php">-Privacy And Policy</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Contact Us</h4>
+            <p><img src="images/gps.png" alt="gps" class="contact-img"> 160/A, Ekambara state, Gaspe.</p>
+            <p><img src="images/phone.png" alt="gps" class="contact-img"> +94 765826516</p>
+            <p><img src="images/email.png" alt="gps" class="contact-img"> info@TechWave.com</p>
+         </div>
 
-    <?php
-        include "footer.php";
-     ?>
-
-    </div>
+         <div class="footer-col">
+            <h4>Follow Us</h4>
+            <div class="links">
+            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+    </footer>
        
     
  
