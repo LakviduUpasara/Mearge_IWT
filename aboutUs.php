7aede8df84e115b57fce41185587bcebd7c91a37
Tnadeeja <thamiyanw@gmail.com>
2024-10-06 06:06:59 +0530

    about us

--- aboutUs.php
+++ aboutUs.php
@@ -2,7 +2,7 @@
 <head>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="styles/aboutus_sylesheet.css" rel="stylesheet"/>
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 
@@ -105,12 +105,35 @@
     </div>
     </div>
 </div>
-<div class ="footer">
+ <!--Footer -->
+ <footer>
+        <div class="footer-col">
+            <h4>Quick Links</h4>
+            <ul>
+            <li><a href="home.php">-Home</a></li>
+            <li><a href="About us.php">-About Us</a></li>
+            <li><a href="contactUs.php">-Conatct Us</a></li>
+            <li><a href="#">-Privacy And Policy</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Contact Us</h4>
+            <p><img src="images/gps.png" alt="gps" class="contact-img"> 160/A, Ekambara state, Gaspe.</p>
+            <p><img src="images/phone.png" alt="gps" class="contact-img"> +94 765826516</p>
+            <p><img src="images/email.png" alt="gps" class="contact-img"> info@TechWave.com</p>
+         </div>
 
-        <?php
-        include "footer.php";
-        ?>
-    
-</div>
+         <div class="footer-col">
+            <h4>Follow Us</h4>
+            <div class="links">
+            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+    </footer>
+
+
 </body>
 </html>
